144d7db829ba75eb9b7fe703c90ff0675b945736
Watcahanan <79349006+robot-eng@users.noreply.github.com>
2022-08-12 12:49:47 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -2,7 +2,7 @@ cd $HOME
 git clone https://github.com/linorobot/lino_install
 cd lino_install 
 sudo rm -rf install
-cp $HOME/robot_install/fix/install.sh $HOME/lino_install/
+cp $HOME/robot_install/fix/install $HOME/lino_install/
 
 echo "Go at $HOME/lino_install/install.sh"
 echo "Then run ./install <base> <sensor>"
