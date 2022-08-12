72259484bedaeed76a06e794178784901fb61a1f
Watcahanan <79349006+robot-eng@users.noreply.github.com>
2022-08-12 12:47:18 +0700

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -6,3 +6,18 @@ cp $HOME/robot_install/fix/install.sh $HOME/lino_install/
 
 echo "Go at $HOME/lino_install/install.sh"
 echo "Then run ./install <base> <sensor>"
+echo
+echo "base The type of robot you want to build. Valid arguments are"
+echo " - 2wd"
+echo " - 4wd"
+echo " - ackermann"
+echo " - mecanum"
+echo "---------------------------------------------------------------"
+echo "The lidar you're going to use for your build. Valid arguments are"
+echo " - xv11"
+echo " - rplidar"
+echo " - ydlidar"
+echo " - hokuyo"
+echo " - kinect"
+echo " - realsense"
+echo
